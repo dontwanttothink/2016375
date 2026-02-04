ebf09b2d83871fd1fa2ca58bd54d54fe19729bbb
dontwanttothink <barquean.09excavacion@icloud.com>
2026-02-04 15:49:48 -0500

    Agregar comentarios

--- src/sketches/1.ts
+++ src/sketches/1.ts
@@ -1,17 +1,18 @@
 import p5 from "p5";
 
-const canvasParent = document.getElementById("p");
-if (!canvasParent) {
-	throw Error();
-}
-
+// Estado
 let dy = 0;
 let dx = 0;
 
+// Configuración del lienzo
 function setup(p: p5) {
-	p.createCanvas(800, 600);
+	p.createCanvas(Math.min(800, innerWidth - 64), 600);
+}
+function windowResized(p: p5) {
+	p.resizeCanvas(Math.min(800, innerWidth - 64), 600);
 }
 
+// Dibujo (cada fotograma)
 function draw(p: p5) {
 	p.background(0);
 	p.fill(255);
@@ -20,6 +21,7 @@ function draw(p: p5) {
 	handleInput(p);
 }
 
+// Responder a las entradas
 function handleInput(p: p5) {
 	if (p.keyIsDown(p.UP_ARROW)) {
 		dy -= 20;
@@ -37,7 +39,17 @@ function handleInput(p: p5) {
 	}
 }
 
+// Inicializar el bosquejo p5
+//
+// (se crea el elemento del lienzo en la página, pasamos nuestras
+// funciones, etc.)
+const canvasParent = document.getElementById("p");
+if (!canvasParent) {
+	throw Error();
+}
+
 new p5((p) => {
 	p.setup = () => setup(p);
 	p.draw = () => draw(p);
+	p.windowResized = () => windowResized(p);
 }, canvasParent);
