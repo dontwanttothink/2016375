c76bf7bffc9f14d55ace5dab8739f0bdcecd64dc
dontwanttothink <barquean.09excavacion@icloud.com>
2026-02-04 11:55:01 -0500

    Organize a bit

--- src/sketches/1.ts
+++ src/sketches/1.ts
@@ -5,36 +5,39 @@ if (!canvasParent) {
 	throw Error();
 }
 
-new p5((p) => {
-	let dy = 0;
-	let dx = 0;
+let dy = 0;
+let dx = 0;
 
-	p.setup = () => {
-		p.createCanvas(800, 600);
-	};
+function setup(p: p5) {
+	p.createCanvas(800, 600);
+}
 
-	p.draw = () => {
-		p.background(0);
-		p.fill(255);
-		p.ellipse(p.width / 2 + dx, p.height / 2 + dy, 100, 100);
+function draw(p: p5) {
+	p.background(0);
+	p.fill(255);
+	p.ellipse(p.width / 2 + dx, p.height / 2 + dy, 100, 100);
 
-		handleInput(p);
-	};
+	handleInput(p);
+}
 
-	function handleInput(p: p5) {
-		if (p.keyIsDown(p.UP_ARROW)) {
-			dy -= 20;
-			console.debug(dy);
-		}
-		if (p.keyIsDown(p.DOWN_ARROW)) {
-			dy += 20;
-			console.debug(dy);
-		}
-		if (p.keyIsDown(p.LEFT_ARROW)) {
-			dx -= 20;
-		}
-		if (p.keyIsDown(p.RIGHT_ARROW)) {
-			dx += 20;
-		}
+function handleInput(p: p5) {
+	if (p.keyIsDown(p.UP_ARROW)) {
+		dy -= 20;
+		console.debug(dy);
 	}
+	if (p.keyIsDown(p.DOWN_ARROW)) {
+		dy += 20;
+		console.debug(dy);
+	}
+	if (p.keyIsDown(p.LEFT_ARROW)) {
+		dx -= 20;
+	}
+	if (p.keyIsDown(p.RIGHT_ARROW)) {
+		dx += 20;
+	}
+}
+
+new p5((p) => {
+	p.setup = () => setup(p);
+	p.draw = () => draw(p);
 }, canvasParent);
