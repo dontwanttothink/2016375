8e8aa5633fa690f34804766b39029d27edd1fdb0
dontwanttothink <barquean.09excavacion@icloud.com>
2026-02-09 14:46:12 -0500

    Agregar código para dibujar celdas
    También organizar todo dentro de estructuras

--- src/sketches/1.ts
+++ src/sketches/1.ts
@@ -1,12 +1,183 @@
 import p5 from "p5";
 import targetDimensions from "../dimensions";
 
-// Estado
-const solution: boolean[][] = [];
-const difficulty = 1;
+// Estructuras de datos
+/**
+ * Una celda dentro de la matriz.
+ */
+class Cell {
+	static ANIMATION_DURATION = 400;
+	static PADDING = 7;
 
-// Configuración del lienzo
+	/**
+	 * @returns El punto en el tiempo actual, medido en milisegundos,
+	 * según el marco de referencia usado por las celdas.
+	 */
+	static currentTime() {
+		return Number(document.timeline.currentTime) + Cell.ANIMATION_DURATION;
+	}
+
+	color: p5.Color;
+	enabled = false;
+
+	/**
+	 * Un punto en el tiempo, medido en milisegundos.
+	 */
+	#lastToggled: number = 0;
+
+	/**
+	 * El progreso de la animación, que usamos al dibujar la celda.
+	 * @returns Un número entre 0 y 1.
+	 */
+	get #progress() {
+		const rawProgress =
+			(Cell.currentTime() - this.#lastToggled) / Cell.ANIMATION_DURATION;
+
+		if (this.enabled) {
+			return Math.min(rawProgress, 1);
+		}
+		return Math.max(1 - rawProgress, 0);
+	}
+
+	/**
+	 * Prende la celda si está apagada. Apaga la celda si está prendida.
+	 */
+	toggle() {
+		this.enabled = !this.enabled;
+		this.#lastToggled = Cell.currentTime();
+	}
+
+	/**
+	 * Dadas las coordenadas de su esquina superior izquierda y su
+	 * tamaño, dibuja la celda.
+	 */
+	draw(p: p5, x: number, y: number, size: number) {
+		size -= Cell.PADDING * 2;
+		p.push();
+
+		const fillColor = p.color(this.color);
+		fillColor.setAlpha(this.#progress * 255);
+
+		p.fill(fillColor);
+		p.noStroke();
+
+		p.rect(x + Cell.PADDING, y + Cell.PADDING, size, size, 10);
+
+		p.pop();
+	}
+
+	constructor(color: p5.Color) {
+		this.color = color;
+	}
+}
+
+interface GridProperties {
+	size: number;
+	startX: number;
+	startY: number;
+	deltaColumn: number;
+	deltaRow: number;
+}
+
+/**
+ * Una matriz.
+ */
+class Grid {
+	p: p5;
+	count: number;
+	#matrix: Cell[][] = [];
+
+	constructor(count: number, p: p5, defaultColor: p5.Color) {
+		this.p = p;
+		this.count = count;
+
+		for (let i = 0; i < count; ++i) {
+			const row: Cell[] = [];
+			for (let j = 0; j < count; ++j) {
+				row.push(new Cell(defaultColor));
+			}
+			this.#matrix.push(row);
+		}
+	}
+
+	static LINE_WIDTH = 2;
+	static LINE_BRIGHTNESS = 200;
+
+	properties(p: p5): GridProperties {
+		const size = Math.min(p.height, p.width) - Grid.LINE_WIDTH;
+		const startX = p.width / 2 - size / 2;
+		const startY = p.height / 2 - size / 2;
+		const deltaRow = size / this.count;
+		const deltaColumn = size / this.count;
+
+		return {
+			size,
+			startX,
+			startY,
+			deltaRow,
+			deltaColumn,
+		};
+	}
+
+	get(column: number, row: number) {
+		return this.#matrix[row][column];
+	}
+
+	draw(p: p5) {
+		p.push();
+
+		const { size, startX, startY, deltaColumn, deltaRow } = this.properties(p);
+
+		for (const [i, row] of this.#matrix.entries()) {
+			for (const [j, cell] of row.entries()) {
+				const y = startY + i * deltaRow;
+				const x = startX + j * deltaColumn;
+
+				cell.draw(p, x, y, size / this.count);
+			}
+		}
+		this.#drawEdges(p);
+
+		p.pop();
+	}
+
+	/**
+	 * Dibuja los bordes de cada fila y columna.
+	 */
+	#drawEdges(p: p5) {
+		p.push();
+
+		p.strokeWeight(Grid.LINE_WIDTH);
+		p.stroke(Grid.LINE_BRIGHTNESS);
+		p.noFill();
+
+		const { size, startX, startY, deltaColumn, deltaRow } = this.properties(p);
+
+		// Bordes
+		p.rect(startX, startY, size, size);
+
+		// Separadores
+		for (let i = 1; i < this.count; ++i) {
+			const x = startX + deltaColumn * i;
+			p.line(x, startY, x, startY + size);
+		}
+		for (let i = 1; i < this.count; ++i) {
+			const y = startY + deltaRow * i;
+			p.line(startX, y, startX + size, y);
+		}
+
+		p.pop();
+	}
+}
+
+// Estado
+const gridSize = 3;
+let grid: Grid;
+
+// Configuración
 function setup(p: p5) {
+	grid = new Grid(gridSize, p, p.color(100));
+
 	const [width, height] = targetDimensions();
 	p.createCanvas(width, height);
 }
@@ -15,46 +186,16 @@ function windowResized(p: p5) {
 	p.resizeCanvas(width, height);
 }
 
-// Utilidades
-function drawGrid(p: p5, count: number) {
-	p.push();
-	const LINE_WIDTH = 2;
-
-	const size = Math.min(p.height, p.width) - LINE_WIDTH;
-
-	const startX = p.width / 2 - size / 2;
-	const startY = p.height / 2 - size / 2;
-
-	const deltaRow = size / count;
-	const deltaColumn = size / count;
-
-	p.strokeWeight(LINE_WIDTH);
-	p.noFill();
-
-	// Bordes
-	p.rect(startX, startY, size, size);
-
-	// Separadores
-	for (let i = 1; i < count; ++i) {
-		const x = startX + deltaColumn * i;
-		p.line(x, startY, x, startY + size);
-	}
-	for (let i = 1; i < count; ++i) {
-		const y = startY + deltaRow * i;
-		p.line(startX, y, startX + size, y);
-	}
-
-	p.pop();
-}
-
 // Dibujo (cada fotograma)
 function draw(p: p5) {
 	p.background(255);
-	p.fill(0);
-	p.ellipse(p.width / 2, p.height / 2, 100, 100);
 
-	p.stroke(200);
-	drawGrid(p, 3);
+	grid.draw(p);
+	if (Math.random() <= 0.025) {
+		const row = Math.floor(Math.random() * grid.count);
+		const column = Math.floor(Math.random() * grid.count);
+		grid.get(column, row).toggle();
+	}
 
 	handleInput(p);
 }
