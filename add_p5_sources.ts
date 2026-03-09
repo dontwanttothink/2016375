549e98384fb875cdc7a8519bb226e365b368de79
dontwanttothink <barquean.09excavacion@icloud.com>
2026-03-08 22:36:26 -0500

    Agregar información sobre derechos de autor y redistribuir el código fuente de p5.js

---
+++ add_p5_sources.ts
@@ -0,0 +1,15 @@
+/// <reference types="@types/bun" />
+
+// Este pequeño script agrega el código fuente de p5, para ofrecer un link de
+// descarga de la versión exacta que se usó.
+
+import { resolve } from "node:path";
+import { create } from "tar";
+
+await create(
+	{
+		gzip: true,
+		file: resolve(import.meta.dirname, "public", "p5-sources.tar.gz"),
+	},
+	[resolve(import.meta.dirname, "node_modules", "p5")],
+);
