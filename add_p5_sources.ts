5e93addd6450583e006965c009e3c62a9957a774
dontwanttothink <barquean.09excavacion@icloud.com>
2026-03-16 20:37:51 -0500

    Agregar quadrille
    También actualizar dependencias

--- add_p5_sources.ts
+++ add_p5_sources.ts
@@ -1,18 +1,27 @@
 /// <reference types="@types/bun" />
 
-// Este pequeño script agrega el código fuente de p5, para ofrecer un link de
-// descarga de la versión exacta que se usó.
+// Este pequeño script agrega el código fuente de p5 y p5.quadrille, para
+// ofrecer un link de descarga de la versión exacta que se usó.
 
 import { resolve } from "node:path";
 import { create } from "tar";
 
-const sources = resolve(import.meta.dirname, "node_modules", "p5");
+const sources = resolve(import.meta.dirname, "node_modules");
 
 await create(
 	{
 		gzip: true,
 		file: resolve(import.meta.dirname, "public", "p5-sources.tar.gz"),
-		cwd: sources,
+		cwd: resolve(sources, "p5"),
+	},
+	["."],
+);
+
+await create(
+	{
+		gzip: true,
+		file: resolve(import.meta.dirname, "public", "p5-quadrille-sources.tar.gz"),
+		cwd: resolve(sources, "p5.quadrille"),
 	},
 	["."],
 );
