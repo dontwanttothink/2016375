bbc71901c2b3e0d3335659f9655d21ce35762c6c
dontwanttothink <barquean.09excavacion@icloud.com>
2026-04-15 12:57:36 -0500

    Más detalles para los tipos

--- types/p5.quadrille.d.ts
+++ types/p5.quadrille.d.ts
@@ -1,9 +1,32 @@
+import type p5 from "p5";
 import type Quadrille from "p5.quadrille";
 
 declare module "./p5.quadrille" {
 	interface p5 {
 		createQuadrille(cols: number, rows: number): Quadrille;
 		createQuadrille(): Quadrille;
-		drawQuadrille(quadrille: Quadrille);
+		drawQuadrille(
+			quadrille: Quadrille,
+			options?:
+				| {
+						cellLength: number;
+						outline: p5.Color;
+						outlineWeight: number;
+						textColor: p5.Color;
+						textZoom: number;
+						textFont: p5.Font;
+						x: number;
+						y: number;
+						row: number;
+						col: number;
+						filter:
+							| Array
+							| Set
+							| ((m: any) => boolean)
+							| { value: any; row: number; col: number };
+						graphics: p5.Graphics;
+				  }
+				| Record<string, any>,
+		);
 	}
 }
