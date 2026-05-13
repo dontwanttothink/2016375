a3bce843c31bf94688fb1a8ab1e91f8d839261b5
dontwanttothink <barquean.09excavacion@icloud.com>
2026-05-13 11:29:02 -0500

    Admitir mouseDragged

--- src/pages.ts
+++ src/pages.ts
@@ -60,7 +60,8 @@ export abstract class Page<TArgs extends PageArgs = undefined> {
 	receive(_args: TArgs) {}
 	setup(_p: p5): void {}
 
-	mouseClicked(_p: p5) {}
+	mouseClicked(_p: p5, _event: MouseEvent) {}
+	mouseDragged(_p: p5, _event: MouseEvent) {}
 	keyPressed(_p: p5) {}
 }
 
@@ -170,8 +171,20 @@ export class Navigator {
 				p.resizeCanvas(width, height);
 			};
 
-			p.mouseClicked = (_event) => {
-				this.#currentPage.mouseClicked(p);
+			p.mouseClicked = (event) => {
+				if (!event) {
+					// nunca debería pasar
+					throw new ReferenceError();
+				}
+				this.#currentPage.mouseClicked(p, event);
+			};
+
+			p.mouseDragged = (event) => {
+				if (!event) {
+					// nunca debería pasar
+					throw new ReferenceError();
+				}
+				this.#currentPage.mouseDragged(p, event);
 			};
 		};
 	}
