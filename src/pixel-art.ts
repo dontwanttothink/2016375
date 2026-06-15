4803733e3667198ff4620ab4c570f565f6cb3d63
dontwanttothink <barquean.09excavacion@icloud.com>
2026-06-14 21:43:35 -0500

    Agregar arte de ejemplo

--- src/pixel-art.ts
+++ src/pixel-art.ts
@@ -8,8 +8,25 @@ function expect<T>(x: T, msg?: string): NonNullable<T> {
 	return x;
 }
 
+class ArtMatrix {
+	constructor(
+		public width: number,
+		public pixels: p5.Color[],
+	) {
+		if (pixels.length % width !== 0) {
+			throw new TypeError(
+				`The number of pixels, ${pixels.length}, is not a multiple of the provided width, ${width}.`,
+			);
+		}
+	}
+
+	get height() {
+		return this.pixels.length / this.width;
+	}
+}
+
 interface ArtAnimation {
-	frames: p5.Color[][];
+	frames: ArtMatrix[];
 	rate: number;
 }
 
@@ -19,9 +36,9 @@ interface ArtAnimationState {
 }
 
 export class Art {
-	static ART_URL = new URL("/art");
+	private static ART_URL = new URL("/art/", window.location.origin);
 
-	static pixelsToColors(p: p5, pixels: Uint8ClampedArray) {
+	private static pixelsToColors(p: p5, pixels: Uint8ClampedArray) {
 		const out: p5.Color[] = [];
 		p.push();
 		p.colorMode(p.RGB, 255);
@@ -39,15 +56,15 @@ export class Art {
 		return out;
 	}
 
-	static intoColorArray(p: p5, url: URL): Promise<p5.Color[]> {
+	private static intoColorArray(p: p5, url: URL): Promise<ArtMatrix> {
 		return new Promise((resolve, reject) => {
 			const canvas = document.createElement("canvas");
 			const context = expect(canvas.getContext("2d"));
 			const image = new Image();
 
 			image.onload = () => {
-				canvas.width = image.width;
-				canvas.height = image.height;
+				canvas.width = image.naturalWidth;
+				canvas.height = image.naturalHeight;
 
 				context.drawImage(image, 0, 0);
 
@@ -57,7 +74,9 @@ export class Art {
 					canvas.width,
 					canvas.height,
 				);
-				resolve(Art.pixelsToColors(p, imageData.data));
+				resolve(
+					new ArtMatrix(canvas.width, Art.pixelsToColors(p, imageData.data)),
+				);
 
 				URL.revokeObjectURL(image.src);
 			};
@@ -71,24 +90,58 @@ export class Art {
 		});
 	}
 
-	static async fromName(p: p5, name: string): Art {
-		const location = new URL(`${name}`, Art.ART_URL);
+	public static async fromName(p: p5, name: string): Promise<Art> {
+		const location = new URL(`${name}/`, Art.ART_URL);
+		console.debug(location);
 		const canonURL = new URL("canon.png", location);
+		console.debug(canonURL);
 
 		const canonical = await Art.intoColorArray(p, canonURL);
 		return new Art(p, location, canonical);
+	}
+
+	private static drawMatrix(
+		p: p5,
+		[x, y]: [number, number],
+		matrix: ArtMatrix,
+		width: number,
+		height: number,
+	) {
+		p.push();
+		const { width: widthInPixels, height: heightInPixels, pixels } = matrix;
+
+		const heightPerPixel = height / heightInPixels;
+		const widthPerPixel = width / widthInPixels;
+
+		p.rectMode(p.CORNERS);
+		for (let i = 0; i < widthInPixels; ++i) {
+			for (let j = 0; j < heightInPixels; ++j) {
+				const color = pixels[widthInPixels * j + i];
+
+				p.noStroke();
+				p.fill(color);
+
+				p.rect(
+					Math.round(x + widthPerPixel * i),
+					Math.round(y + heightPerPixel * j),
+					Math.round(x + widthPerPixel * (i + 1)),
+					Math.round(y + heightPerPixel * (j + 1)),
+				);
+			}
+		}
+		p.pop();
 	}
 
 	private p: p5;
 
 	private location: URL;
 
-	private canonical: p5.Color[];
+	private canonical: ArtMatrix;
 	private animations: Map<string, ArtAnimation> = new Map();
 
-	animation: ArtAnimationState | null = null;
+	private animation: ArtAnimationState | null = null;
 
-	private constructor(p: p5, location: URL, canonical: p5.Color[]) {
+	private constructor(p: p5, location: URL, canonical: ArtMatrix) {
 		this.p = p;
 		this.location = location;
 		this.canonical = canonical;
@@ -108,11 +161,11 @@ export class Art {
 		if (typeof rate !== "number") {
 			throw new TypeError();
 		}
-		if (typeof frameCount === "number") {
+		if (typeof frameCount !== "number") {
 			throw new TypeError();
 		}
 
-		const frames: p5.Color[][] = [];
+		const frames: ArtMatrix[] = [];
 		for (let i = 0; i < frameCount; ++i) {
 			const animLocator = new URL(`${i}.png`, animationURL);
 			frames.push(await Art.intoColorArray(this.p, animLocator));
@@ -126,7 +179,9 @@ export class Art {
 		this.animations.set(name, animation);
 	}
 
-	draw() {}
+	draw([x, y]: [number, number], width: number, height: number) {
+		Art.drawMatrix(this.p, [x, y], this.canonical, width, height);
+	}
 }
 
 // export function Art(p: p5, pixels: Uint8ClampedArray, width: number): Art {
--- src/sketches/3/index.ts
+++ src/sketches/3/index.ts
@@ -1,10 +1,17 @@
 import p5 from "p5";
 import { Navigator, Page } from "../../pages";
+import { Art } from "../../pixel-art";
 
 // nota: iterar sobre entidades para dibujar, en vez de tener un arreglo
 // bidimensional
 
 class WelcomePage extends Page {
+	exampleArt!: Art;
+
+	async preload(p: p5) {
+		this.exampleArt = await Art.fromName(p, "protagonist");
+	}
+
 	setup(p: p5) {
 		p.noStroke();
 		p.fill("rebeccapurple");
@@ -13,6 +20,11 @@ class WelcomePage extends Page {
 	draw(p: p5) {
 		p.clear();
 		p.circle(p.width / 2, p.height / 2, 200);
+		this.exampleArt.draw(
+			[p.mouseX, p.mouseY],
+			400,
+			400 + Math.sin(p.millis() / 200) * 30,
+		);
 	}
 }
 
@@ -24,4 +36,4 @@ if (!canvasParent) {
 }
 
 new p5(navigator.sketch, canvasParent);
-p5.disableFriendlyErrors = true; // falsos positivos
+p5.disableFriendlyErrors = true; // demasiados falsos positivos
