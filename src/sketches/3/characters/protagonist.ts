120997102ef6c72afe5ee4c480d97f005f2a918e
dontwanttothink <bcuentas@unal.edu.co>
2026-07-01 23:25:36 -0500

    Agregar clase para el protagonista

---
+++ src/sketches/3/characters/protagonist.ts
@@ -0,0 +1,12 @@
+import type p5 from "p5";
+import { Entity } from "../entity";
+import { EntityArt } from "../entity/art";
+
+class ProtagonistEntity extends Entity {}
+
+export async function Protagonist(p: p5, location: [number, number]) {
+	const art = await EntityArt.fromName(p, "example");
+	art.loadAnimation("blink");
+
+	return new ProtagonistEntity(art, location);
+}
