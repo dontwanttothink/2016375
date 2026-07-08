3a0b203a8ce001cc97e9a8e3013f204def3d43aa
dontwanttothink <bcuentas@unal.edu.co>
2026-07-08 12:51:26 -0500

    Eliminar movimiento por teclado otra vez

--- src/sketches/3/characters/protagonist.ts
+++ src/sketches/3/characters/protagonist.ts
@@ -2,23 +2,7 @@ import type p5 from "p5";
 import { Entity } from "../entity";
 import { EntityArt } from "../entity/art";
 
-export class ProtagonistEntity extends Entity {
-	up() {
-		this.displace([0, -1]);
-	}
-
-	down() {
-		this.displace([0, 1]);
-	}
-
-	left() {
-		this.displace([-1, 0]);
-	}
-
-	right() {
-		this.displace([1, 0]);
-	}
-}
+export class ProtagonistEntity extends Entity {}
 
 export async function Protagonist(p: p5, location: [number, number]) {
 	const art = await EntityArt.fromName(p, "example");
