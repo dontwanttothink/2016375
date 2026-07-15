e98826423cf537158d94faf6feda5b77e9f83f88
dontwanttothink <bcuentas@unal.edu.co>
2026-07-15 12:03:02 -0500

    Darle su nombre completo a Musgonzalo

--- src/sketches/3/characters/musgo.ts
+++ src/sketches/3/characters/musgo.ts
@@ -5,7 +5,7 @@ import { EnemyEntity } from "./enemy";
 export class MusgoEntity extends EnemyEntity {
 	constructor(p: p5, art: EntityArt, position: [number, number]) {
 		super(p, art, position);
-		this.name = "Musgo";
+		this.name = "Musgonzalo";
 	}
 }
 
