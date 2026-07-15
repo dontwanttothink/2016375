9e10d43888321c07adfc5e76fd1fbad51936f921
Sergio <sergioacosta792m@gmail.com>
2026-07-14 19:58:18 -0500

    Enemigo araña

---
+++ src/sketches/3/characters/araña.ts
@@ -0,0 +1,16 @@
+import type p5 from "p5";
+import { EntityArt } from "../entity/art";
+import { CombatantEntity } from "./combatant";
+
+export class ArañaEntity extends CombatantEntity {
+    constructor(p: p5, art: EntityArt, position: [number, number]) {
+        super(p, art, position);
+        this.name = "Araña";
+        this.isMovable = false;
+    }
+}
+
+export async function Araña(p: p5, position: [number, number] = [0, 0]) {
+    const art = await EntityArt.fromName(p, "araña");
+    return new ArañaEntity(p, art, position);
+}
