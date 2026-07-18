43503dc4f5680ebc7a4e774bf24ad9634c93cd29
dontwanttothink <bcuentas@unal.edu.co>
2026-07-18 15:01:11 -0500

    Limpiar

--- src/sketches/3/characters/slime.ts
+++ src/sketches/3/characters/slime.ts
@@ -2,14 +2,14 @@ import type p5 from "p5";
 import { EntityArt } from "../entity/art";
 import { EnemyEntity } from "./enemy";
 
-export class ArañaEntity extends EnemyEntity {
+export class SlimeEntity extends EnemyEntity {
 	constructor(p: p5, art: EntityArt, position: [number, number]) {
 		super(p, art, position);
-		this.name = "slime";
+		this.name = "Slime";
 	}
 }
 
-export async function slime(p: p5, position: [number, number] = [0, 0]) {
+export async function Slime(p: p5, position: [number, number] = [0, 0]) {
 	const art = await EntityArt.fromName(p, "slime");
-	return new ArañaEntity(p, art, position);
+	return new SlimeEntity(p, art, position);
 }
